ae5bccb67c9550d65c2dcd21a63b8cef0454a691
holeeeeeee12 <hand090399@gmail.com>
2025-09-29 09:27:33 +0900

    20250929_수정1

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,25 +1,22 @@
 import React from "react";
+import Rule from "./componets/JSX/Rule";
+import Interpolation from "./componets/JSX/interpolation";
+import Header from "./componets/Header";
+import Footer from "./componets/Footer";
+import Header from "./componets/Header";
+import MyButton from "./componets/MyButton";
+import MyList from "./componets/MyList";
 
 export default function App() {
-  // DOM API (주석): textContent 철자 주의!
-  // const element = document.createElement("h1");
-  // element.textContent = "App";
-
   return (
-    <>
-      <div>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        {/* 규칙2. 모든 태그는 반드시 닫아야 한다 */}
-        {/* input 태그는 self-closing */}
-        <input />
-        {/* 규칙3. 속성명은 카멜케이스로 작성 (onClick, onSubmit 등) */}
-        {/* class -> className */}
-      </div>
-
-      <p className="font-bold text-5xl text-red-700">p 태그</p>
-      <p style={{ fontSize: "3rem", color: "blue" }}>p 태그 2</p>
-    </>
+    <div>
+      {/* {Interpolation 컴포넌트 불러오기} */}
+      <Header></Header>
+      <Interpolation> </Interpolation>
+      <MyButton></MyButton>
+      <MyList></MyList>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
   );
 }
---
+++ my-react-app/src/componets/Header.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Interpolation from "./JSX/interpolation";
+
+export default function Headerader() {
+  return <div>Header</div>;
+}
