8cf1a2b905a5427360911fce45b85545de97ff40
holeeeeeee12 <hand090399@gmail.com>
2025-10-16 09:15:02 +0900

    추가

--- react/my-react-router-app/redux-app/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import counterReducer from "./counterSlices";
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
